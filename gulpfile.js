195fc2701259769d4b2535a3eb588c10f198a0d6
Stephen Cox <stephen@agile.coop>
2017-02-04 14:26:16 +0000

    Moved gulpfile to ES6 syntax

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,21 @@
  * Gulp file for colour tools site
  */
 
+'use strict';
+
 /**
  * Config
  */
-var sass_conf = {
+const sass_conf = {
   outputStyle: 'nested',
   includePaths: [
-    'web/lib/font-awesome/scss'
+    'web/lib/font-awesome/scss',
   ],
   precision: 10,
-  errLogToConsole: true
+  errLogToConsole: true,
 };
-var prefix_conf = {
-  browsers: ['last 2 versions', 'ie > 8']
+const prefix_conf = {
+  browsers: ['last 2 versions', 'ie > 8'],
 };
 
 /**
@@ -22,41 +24,40 @@ var prefix_conf = {
  */
 
 // Gulp / Node utilities
-var gulp = require('gulp-help')(require('gulp'));
-var u = require('gulp-util');
-var log = u.log;
-var c = u.colors;
-var del = require('del');
-var plumber = require('gulp-plumber');
-var argv = require('yargs').default('production', false).argv;
-var watch = require('gulp-watch');
+const gulp = require('gulp-help')(require('gulp'));
+const u = require('gulp-util');
+const log = u.log;
+const c = u.colors;
+const del = require('del');
+const plumber = require('gulp-plumber');
+const argv = require('yargs').default('production', false).argv;
+const watch = require('gulp-watch');
 
 // SASS
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var prefix = require('gulp-autoprefixer');
-var clean = require('gulp-clean-css');
-var globbing = require('gulp-css-globbing');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const prefix = require('gulp-autoprefixer');
+const clean = require('gulp-clean-css');
+const globbing = require('gulp-css-globbing');
 
 // JavaScript
-var concat = require('gulp-concat');
-var babel = require('gulp-babel');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
 
 // Images
-var svgmin = require('gulp-svgmin');
-var gzip = require('gulp-gzip');
+const imagemin = require('gulp-imagemin');
 
 // Nunjucks templating
-var nunjucks = require('gulp-nunjucks-render');
+const nunjucks = require('gulp-nunjucks-render');
 
 // Browser Sync
-var bs = require('browser-sync').create();
+const bs = require('browser-sync').create();
 
 
 // Check environment
-var env = 'dev';
+let env = 'dev';
 if (argv.production) {
   env = 'prod';
 }
@@ -64,33 +65,32 @@ if (argv.production) {
 /**
  * Error handler
  */
-var onError = function (err) {
+function onError(err) {
 
   log(c.red(err.message));
-  if (err.message != 'undefined') {
-    log(c.red("Error: " + err.message));
-  }
-  else if (err.messageFormatted) {
+  if (err.message !== 'undefined') {
+    log(c.red(`Error: ${err.message}`));
+  } else if (err.messageFormatted) {
     // Send the error to the console.
-    log(c.red("Error in file: " + err.messageFormatted));
+    log(c.red(`Error in file: ${err.messageFormatted}`));
   }
-  this.emit('end')
-};
+  this.emit('end');
+}
 
 /**
  * Compile SASS for development environment
  */
-gulp.task('sass-dev', 'Compile SASS for development environment', function () {
+gulp.task('sass-dev', 'Compile SASS for development environment', () => {
 
   return gulp.src([
-      'sass/**/*.scss'
+      'sass/**/*.scss',
     ],
     { base: 'sass' })
     .pipe(plumber({
-      errorHandler: onError
+      errorHandler: onError,
     }))
     .pipe(globbing({
-      extensions: ['.scss']
+      extensions: ['.scss'],
     }))
     .pipe(sourcemaps.init())
     .pipe(sass(sass_conf))
@@ -103,37 +103,36 @@ gulp.task('sass-dev', 'Compile SASS for development environment', function () {
 /**
  * Compile SASS for production environment
  */
-gulp.task('sass-prod', 'Compile SASS for production environment', function () {
+gulp.task('sass-prod', 'Compile SASS for production environment', () => {
 
-  sass_conf.outputStyle  = 'compressed';
+  sass_conf.outputStyle = 'compressed';
   return gulp.src([
-      'sass/**/*.scss'
+      'sass/**/*.scss',
     ],
     { base: 'sass' })
     .pipe(plumber({
-      errorHandler: onError
+      errorHandler: onError,
     }))
     .pipe(globbing({
-      extensions: ['.scss']
+      extensions: ['.scss'],
     }))
     .pipe(sass(sass_conf))
     .pipe(prefix(prefix_conf))
-    .pipe(clean({compatibility: 'ie8'}))
+    .pipe(clean({ compatibility: 'ie8' }))
     .pipe(gulp.dest('./web/css'))
-    .pipe(bs.stream({match: '**/*.css'}));
+    .pipe(bs.stream({ match: '**/*.css' }));
 });
 
 /**
  * Delete compiled CSS
  */
-gulp.task('clean-css', 'Delete all the CSS files in the ./web/css directory', function() {
+gulp.task('clean-css', 'Delete all the CSS files in the ./web/css directory', () => {
 
-  return del(['web/css/*.css'], function (err) {
+  return del(['web/css/*.css'], (err) => {
     if (err) {
       log(c.red('clean-css'), err);
-    }
-    else {
-      log(c.green('clean-css'),'deleted old stylesheets');
+    } else {
+      log(c.green('clean-css'), 'deleted old stylesheets');
     }
   });
 });
@@ -141,11 +140,11 @@ gulp.task('clean-css', 'Delete all the CSS files in the ./web/css directory', fu
 /**
  * Transpile, minify and concatenate Javascript files
  */
-gulp.task('scripts', function() {
+gulp.task('scripts', () => {
 
   return gulp.src('scripts/**/*.js')
     .pipe(plumber({
-      errorHandler: onError
+      errorHandler: onError,
     }))
     .pipe(babel({ presets: ['es2015'] }))
     .pipe(concat('scripts.js'))
@@ -153,69 +152,68 @@ gulp.task('scripts', function() {
     .pipe(rename('scripts.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./web/js'))
-    .pipe(bs.stream({match: '**/*.js'}));
+    .pipe(bs.stream({ match: '**/*.js' }));
 });
 
 /**
- * Compress SVGs
+ * Compress Images
  */
-gulp.task('images', function () {
-  return gulp.src('images/**/*.svg')
+gulp.task('images', 'Compress image in ./images directory', () => {
+
+  return gulp.src('images/**/*.+(gif|jpeg|jpg|png|svg)')
     .pipe(plumber({
-      errorHandler: onError
+      errorHandler: onError,
     }))
-    .pipe(svgmin({
+    .pipe(imagemin({
       plugins: [
-        { removeDoctype: false }
+        { removeDoctype: false },
     ]}))
-//    .pipe(gzip({ append: false }))
-//    .pipe(rename({ extname: '.svgz'}))
     .pipe(gulp.dest('./web/img'));
 });
 
 /**
  * Compile Nunjucks templates
  */
-gulp.task('nunjucks', 'Compile Nunjucks templates in the ./pages directory', function() {
+gulp.task('nunjucks', 'Compile Nunjucks templates in the ./pages directory', () => {
 
   return gulp.src('pages/*.+(html|nunjucks)')
     .pipe(plumber({
-      errorHandler: onError
+      errorHandler: onError,
     }))
     .pipe(nunjucks({
-      path: ['pages/templates']
+      path: ['pages/templates'],
     }))
     .pipe(gulp.dest('web'))
-    .pipe(bs.stream({match: '**/*.html'}));
+    .pipe(bs.stream({ match: '**/*.html' }));
 });
 
 /**
  * Set up Browser Sync
  */
-gulp.task('bs', 'Set Browser Sync to serve the ./web directory', function() {
+gulp.task('bs', 'Set Browser Sync to serve the ./web directory', () => {
   bs.init({
     server: {
-      baseDir: "./web/"
-    }
+      baseDir: './web/',
+    },
   });
 });
 
 /**
  * Watch SASS directory for changes.
  */
-gulp.task('watch-sass', function() {
+gulp.task('watch-sass', () => {
 
-  watch('sass/**/*.scss', {verbose: true, usePolling: true, useFsEvents: true}, function() {
-    gulp.start('sass-' + env);
+  watch('sass/**/*.scss', { verbose: true, usePolling: true, useFsEvents: true }, () => {
+    gulp.start(`sass-${env}`);
   });
 });
 
 /**
  * Watch JavaScript directory for changes.
  */
-gulp.task('watch-scripts', function() {
+gulp.task('watch-scripts', () => {
 
-  watch('scripts/**/*.js', {verbose: true, usePolling: true, useFsEvents: true}, function() {
+  watch('scripts/**/*.js', { verbose: true, usePolling: true, useFsEvents: true }, () => {
     gulp.start('scripts');
   });
 });
@@ -223,9 +221,9 @@ gulp.task('watch-scripts', function() {
 /**
  * Watch images directory for changes.
  */
-gulp.task('watch-images', function() {
+gulp.task('watch-images', () => {
 
-  watch('images/**/*.svg', {verbose: true, usePolling: true, useFsEvents: true}, function() {
+  watch('images/**/*.svg', { verbose: true, usePolling: true, useFsEvents: true },() => {
     gulp.start('images');
   });
 });
@@ -233,9 +231,9 @@ gulp.task('watch-images', function() {
 /**
  * Watch Nunjucks pages directory for changes.
  */
-gulp.task('watch-pages', function() {
+gulp.task('watch-pages', () => {
 
-  watch('pages/**/*.+(html|nunjucks)', {verbose: true, usePolling: true, useFsEvents: true}, function() {
+  watch('pages/**/*.+(html|nunjucks)', { verbose: true, usePolling: true, useFsEvents: true }, () => {
     gulp.start('nunjucks');
   });
 });
@@ -243,4 +241,4 @@ gulp.task('watch-pages', function() {
 /**
  * Default task - compile and watch SASS, JavaScript and Nunjucks templates
  */
-gulp.task('default', false, ['bs', 'sass-' + env, 'scripts', 'images', 'nunjucks', 'watch-sass', 'watch-scripts', 'watch-images', 'watch-pages']);
+gulp.task('default', false, ['bs', 'scripts', 'images', 'nunjucks', `sass-${env}`, 'watch-scripts', 'watch-images', 'watch-pages', 'watch-sass']);
